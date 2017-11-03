fb17a4eb218d176834c17446667ecf581e530782
Zhenfeng <31424829+zfchen95@users.noreply.github.com>
2017-11-02 22:46:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
-import sqlparse
+import sys
+import sqlparse as sp
+import numpy as np
 
 
 def query():
@@ -53,13 +55,85 @@ def loader(filename):
     return count
 
 
+def average(filename):
+    return
+
+
+def sum(filename, attr):
+    myFile = open(filename, 'r')
+    reader = csv.reader(myFile)
+    attrs = next(reader)
+    idx = []
+    for str in attr:
+        idx.append(attrs.index(str))
+    res = np.zeros(len(attr))
+    for row in reader:
+        for i in range(len(attr)):
+            res += float(row[idx[i]])
+    myFile.close()
+    return res
+
+
+def minimum(filename, attr):
+    myFile = open(filename, 'r')
+    reader = csv.reader(myFile)
+    attrs = next(reader)
+    idx = []
+    for str in attr:
+        idx.append(attrs.index(str))
+    res = np.zeros(len(attr))
+    for row in reader:
+        for i in range(len(attr)):
+            res += float(row[idx[i]])
+    myFile.close()
+    return res
+
+
+def maximum(filename, attr):
+    myFile = open(filename, 'r')
+    reader = csv.reader(myFile)
+    attrs = next(reader)
+    idx = []
+    for str in attr:
+        idx.append(attrs.index(str))
+    res = np.zeros(len(attr))
+    for row in reader:
+        for i in range(len(attr)):
+            tmp = float(row[idx[i]])
+            if tmp > res[i]:
+                res[i] = tmp
+    myFile.close()
+    return res
+
+
+def count():
+    return COUNT
+
+
+def find(filename, attr, cond):
+    myFile = open(filename, 'r')
+    reader = csv.reader(myFile)
+    attrs = next(reader)
+    res = []
+    idx = attrs.index(attr)
+    for row in reader:
+        if float(row[idx]) > cond:
+            res.append(row)
+    myFile.close()
+    return res
+
+
 filename = 'players_stats_2014_2015.csv'
 COUNT = loader(filename)
 # print(COUNT)
 # project(filename, ['Name', 'Games Played'])
 # select(filename, 'James Harden')
 sql = 'select * from players_stats_2014_2015.csv where id in (select id from bar)'
-print(sqlparse.format(sql, reindent=True, keyword_case='upper'))
-res = sqlparse.parse(sql)
+print(sp.format(sql, reindent=True, keyword_case='upper'))
+res = sp.parse(sql)
 for str in res:
     print(str)
+
+print(sum(filename, ['3PM']))
+print(maximum(filename, ['3PM']))
+print(find(filename, '3PA', 600))
