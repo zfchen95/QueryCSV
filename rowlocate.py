238d36126c17ed76e67b48116407948e629fbd47
Xi Li <lixi220@outlook.com>
2017-12-20 18:24:42 -0600

    Add files via upload

--- rowlocate.py
+++ rowlocate.py
@@ -3,7 +3,32 @@ import sys
 import io
 import time
 
-def getloc(fname):
+
+def getloc_b_c(fname):
+    #fname is a string. ex:"review-5k.csv"
+    f = open(fname, "r")
+    od = []
+
+    while 1:
+        od.append(f.tell())
+        line = f.readline()
+        if not line:
+            break
+
+    newod = od[0:len(od)-1]
+    #print("len(newod)",len(newod))
+    return(newod)
+    
+def getrow(fname, loclist, num):
+    #fname is a string. loclist is the list from getloc, num is the row number to locate
+    f = open(fname, "r")
+    f.seek(loclist[num])
+    reader = csv.reader(f)
+    res = next(reader)
+    f.close()
+    return(res)
+
+def getloc_r_p(fname):
     #fname is a string. ex:"review-5k.csv"
     f = io.open(fname, encoding = "utf8", newline="\r\n")
     od = []
@@ -14,34 +39,52 @@ def getloc(fname):
         if not line:
             break
 
-    newod = od[0:len(od)-1]
-
-    f.seek(0)
-    reader = csv.reader(f)
-    attr = next(reader)
     f.close()
+    newod = od[0:len(od)-1]
+    
+    flag = [0] * len(newod)
+    
+    f = open(fname, "r")
+    reader = csv.reader(f)
+    row = next(reader)
+    for i in range(len(newod)):
+        fcopy = open(fname, "r")
+        fcopy.seek(newod[i])
+        xreader = csv.reader(fcopy)
+        tmp = next(xreader)
+        fcopy.close()
+        if row!=tmp:
+            flag[i] = 1
+            continue
+        elif i!=len(newod)-1:
+            row = next(reader)
+            
+    f.close()
+        
     finalod = []
     for i in range(len(newod)):
-        x = newod[i]
-        f = io.open(fname, encoding = "utf8", newline="\r\n")
-        f.seek(x)
-        reader = csv.reader(f)
-        tmp = next(reader)
-        if len(tmp)==len(attr):
-            finalod.append(x)
-            f.close()
-    return(finalod)
+        if flag[i]==0:
+            finalod.append(newod[i])
     
-def getrow(fname, loclist, num):
-    #fname is a string. loclist is the list from getloc, num is the row number to locate
-    f = io.open(fname, encoding = "utf8", newline="\r\n")
-    f.seek(loclist[num])
-    reader = csv.reader(f)
-    res = next(reader)
-    f.close()
-    return(res)
+    #print("len(finalod)", len(finalod))
+    return(finalod)
 
-ff = "review-5k.csv"
-flist = getloc(ff)
-ans = getrow(ff,flist,5002)
+fname = "business.csv"
+f = open(fname, "r")
+reader = csv.reader(f)
+bcount = 0
+for row in reader:
+    bcount = bcount+1
+    
+print("bcount", bcount)
+f.close()
+
+#flist = getloc_r_p(fname)
+flist = getloc_b_c(fname)
+
+start = time.time()
+ans = getrow(fname,flist,5002)
 print(ans)
+print(time.time()-start)
+
+
